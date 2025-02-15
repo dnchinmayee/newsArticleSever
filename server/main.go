9e41454e0f9a12927e3d0ef68da7455ddec18bc0
Chinmayee Ojha <Chinmayee.Ojha@gmail.com>
2025-02-15 15:20:04 -0500

    fix: modified server embedding

--- server/main.go
+++ server/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"article-server/controllers"
 	"embed"
+	"io/fs"
+	"net/http"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
+
+type embedFS struct {
+	http.FileSystem
+}
 
 const buildDir = "dist"
 
@@ -16,11 +22,27 @@ var staticFS embed.FS
 func main() {
 	router := gin.Default()
 
-	router.Use(static.Serve("/", static.LocalFile(buildDir, true)))
+	// router.Use(static.Serve("/", static.LocalFile(buildDir, true)))
 
 	newsController := controllers.NewArticleController()
 
 	router.GET("/api/news", newsController.GetNews)
 
+	staticFileSubDir, err := fs.Sub(staticFS, buildDir)
+	if err != nil {
+		panic(err)
+	}
+	staticFileFS := http.FS(staticFileSubDir)
+	router.Use(static.Serve("/", embedFS{staticFileFS}))
+
 	router.Run(":8080")
+}
+
+func (e embedFS) Exists(prefix string, name string) bool {
+	f, err := e.Open(name)
+	if err != nil {
+		return false
+	}
+	f.Close()
+	return true
 }
